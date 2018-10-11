b38f0c91d5c76308a6dd7472f414f7f0c167a4b4
RafaelMiranda21 <38355955+RafaelMiranda21@users.noreply.github.com>
2018-10-11 20:05:13 -0300

    Update Cnpj.h

--- Trab - Oleg cpf cnpj/Cnpj.h
+++ Trab - Oleg cpf cnpj/Cnpj.h
@@ -1,1 +1,18 @@
+#ifndef CNPJ_H
+#define CNPJ_H
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+
+
+class Cnpj
+{
+private:
+    string Cnpj;
+public:
+    Cnpj();
+    static bool valCnpj(string);
+};
+#endif // CNPJ_H
